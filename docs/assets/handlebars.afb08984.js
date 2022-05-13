5694735a18915fd5ab22f5d028d7432c079aa9e9
busypeoples <busypeoples@gmail.com>
2022-05-13 17:57:07 +0200

    Make paths relative

--- docs/assets/handlebars.afb08984.js
+++ docs/assets/handlebars.afb08984.js
@@ -1,4 +1,4 @@
-import{m as i}from"./index.e0237342.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./index.cd7712cf.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
--- docs/assets/javascript.c1f2cfc1.js
+++ docs/assets/javascript.c1f2cfc1.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript.6fc2bc97.js";import"./index.e0237342.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript.71df248a.js";import"./index.cd7712cf.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
